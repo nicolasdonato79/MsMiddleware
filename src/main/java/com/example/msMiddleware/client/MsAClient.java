821bf9524d8748d9eb1dc54149a18add89dfa532
nicolas <ndonato@sms-sudamerica.com>
2025-05-15 09:28:48 -0300

    Actualizacion de endopoints

--- src/main/java/com/example/msMiddleware/client/MsAClient.java
+++ src/main/java/com/example/msMiddleware/client/MsAClient.java
@@ -2,15 +2,25 @@ package com.example.msMiddleware.client;
 
 import com.example.msMiddleware.dto.UserDetailDTO;
 import com.example.msMiddleware.entity.UserDetail;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 
 // Cliente hacia MS A
-@Component
-public class MsAClient {
-    private final RestTemplate rest = new RestTemplate();
+//@Component
+//public class MsAClient {
+//    private final RestTemplate rest = new RestTemplate();
+//
+//    public void syncToMsA(UserDetailDTO userDetailDTO) {
+//        rest.postForEntity("http://localhost:8080/msa/users-details/sync-from-legacy", userDetailDTO, Void.class);
+//    }
+//}
 
-    public void syncToMsA(UserDetailDTO userDetailDTO) {
-        rest.postForEntity("http://localhost:8080/msa/users-details/sync-from-legacy", userDetailDTO, Void.class);
-    }
+@FeignClient(name = "ms-a-client", url = "http://localhost:8080/msa/users-details")
+public interface MsAClient {
+
+    @PostMapping("/sync-from-legacy")
+    void syncToMsA(@RequestBody UserDetailDTO userDetailDTO);
 }
